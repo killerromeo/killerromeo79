6a117ed1f16a845c467165f330cbdaf5191139f0
killerromeo <93936624+killerromeo@users.noreply.github.com>
2021-11-09 17:31:40 +0530

    Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. Use dictionaries.

---
+++ Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency use dictionaries.py
@@ -0,0 +1,10 @@
+a= input('Please enter a string: ')
+def mostfrequent(string):
+  d = dict()
+  for key in string:
+    if key not in d:
+      d[key] = 1
+    else:
+      d[key] += 1
+  return d
+print (mostfrequent(a))
