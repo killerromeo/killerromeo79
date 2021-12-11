1571baf0eb5e8c6d4fd0980fcb2c56de1d2cd5df
killerromeo <93936624+killerromeo@users.noreply.github.com>
2021-12-11 16:25:35 +0530

    Different Set Operations created by using python
    Python Program to Illustrate Different Set Operations Here we have to define two set variables and we have to perform different set operations: union, intersection, difference and symmetric difference.

---
+++ Different Set Operations.py
@@ -0,0 +1,15 @@
+#Write a Python Program to Illustrate Different Set Operations Here we have to define two set variables and we have to perform different set operations: union, intersection, difference and symmetric difference.
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+# set union
+print("Union of E and N is=",E | N)
+
+# set intersection
+print("Intersection of E and N is=",E & N)
+
+# set difference
+print("Difference of E and N is=",E - N)
+
+# set symmetric difference
+print("Symmetric difference of E and N is=",E ^ N)
